2958b4ec7dfbd59f65ba69af4a7ce200111495e0
Siddhartha Roy <sid86harth@gmail.com>
2022-04-25 11:55:07 +0530

    makeId function

--- pages/api/shortUrl.js
+++ pages/api/shortUrl.js
@@ -1,7 +1,18 @@
 import MongoClient from '../../utils/mongodb'
-const Parser = require('shortid')
 
 async function handler(req, res) {
+
+    function makeId(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * 
+     charactersLength));
+       }
+       return result;
+    }
+    
 
     if (req.method !== 'POST') {
         res.status(500).json({ message: 'Soory this is post route' })
@@ -14,7 +25,7 @@ async function handler(req, res) {
     }
 
     const fullUrl = req.body.fullUrl
-    const shortId = await Parser.generate()
+    const shortId = await makeId(7)
 
     const data = {
         _id: shortId,

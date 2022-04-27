442251e0d1dbcc65945903a51bcb51d51173c4c3
sid86-dev <sid86harth@gmail.com>
2022-04-28 04:41:16 +0530

    SEO and favicons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <header className="d-flex justify-content-center py-3">
             <ul className="nav nav-pills">
                 <li className="nav-item"><Link  href="/"><a className="nav-link active" aria-current="page">Home</a></Link></li>
-                <li className="nav-item"><a href="#" className="nav-link">Source</a></li>
+                <li className="nav-item"><a target="_blank" rel="noreferrer" href="https://github.com/sid86-dev/shwt-serverless" className="nav-link">Source</a></li>
                 <li className="nav-item"><Link  href="/test"><a className="nav-link">Pricing</a></Link></li>
                 <li className="nav-item"><a target="_blank" rel="noreferrer" href="https://qrgen-beta.vercel.app/" className="nav-link">Qr Gen</a></li>
 

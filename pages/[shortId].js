4e869ba8e2e90c5214d346ec86527556c2938e72
Siddhartha Roy <sid86harth@gmail.com>
2022-05-22 01:18:23 +0530

    Update [shortId].js

--- pages/[shortId].js
+++ pages/[shortId].js
@@ -1,26 +1,23 @@
+RedirectUrl.getInitialProps = async (ctx) => {
+    const { query, res } = ctx;
 
-export async function getServerSideProps(context) {
-
-    const shortId = context.query.shortId;
+    const shortId = await query.shortId;
 
     const url = `${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/getUrl/${shortId}`
+
+    console.log(url)
 
     const response = await fetch(url);
     const data = await response.json();
 
-    return {
-        redirect: {
-            destination: data.fullUrl || `/invalid?timeStamp=${new Date().getTime()}`,
-            permanent: true
-        }
+
+
+    if (res) {
+        res.writeHead(302, { // or 301
+            Location: data.fullUrl || `/invalid?timeStamp=${new Date().getTime()}`,
+        });
+        res.end();
     }
+
+    return { shortId: shortId }
 }
-
-export default function RedirectUrl({ shortId }) {
-
-    return (<div>
-        <h1>{shortId}</h1>
-    </div>)
-}
-
-
